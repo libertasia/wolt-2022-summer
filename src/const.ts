2ba3eb57da9ee972c67125f50e344f77c1271a87
Anastasiia Erokhina <erokhinaanastasiia@gmail.com>
2022-01-26 18:34:30 +0200

    added const

---
+++ src/const.ts
@@ -0,0 +1,27 @@
+export const FREE_DELIVERY_FEE: number = 0;
+
+export const MAX_DELIVERY_FEE: number = 15;
+
+export const SMALL_CART_VALUE: number = 10;
+
+export const BIG_CART_VALUE: number = 100;
+
+export const FRIDAY_RUSH_MULTIPLIER: number = 1.1;
+
+export const FRIDAY_RUSH_DAY: number = 5;
+
+export const FRIDAY_RUSH_HOUR_START: number = 15;
+
+export const FRIDAY_RUSH_HOUR_END: number = 19;
+
+export const ITEMS_AMOUNT_WITHOUT_SURCHARGE: number = 4;
+
+export const SURCHARGE_PER_ITEM: number = 0.5;
+
+export const BASE_DELIVERY_FEE: number = 2;
+
+export const SURCHARGE_PER_DISTANCE_STEP: number = 1;
+
+export const DELIVERY_DISTANCE_STEP: number = 500;
+
+export const BASE_DISTANCE: number = 1000;
